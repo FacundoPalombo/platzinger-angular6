f747784e6633c7d70be0b82798d276154fb42065
Facundo Palombo <facundo.palombo@gmail.com>
2019-01-17 19:33:52 -0300

    Se crean mas usuarios para probar el componente de home y se muestran

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { User } from '../interfaces/user';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  friends: User[];
   constructor() {
     let myUser: User = {
       nick: 'Facundo',
@@ -17,8 +17,44 @@ export class HomeComponent implements OnInit {
       friend: true,
       uid: 1
     };
-    let users: User[] = [
-      myUser
+    let eduardo: User = {
+      nick: 'Eduardo',
+      subnick: 'Hola! Soy Eduardo!',
+      email: 'eduard@gmail.com',
+      age: 24,
+      friend: false,
+      uid: 2
+    };
+    let jorge: User = {
+      nick: 'Jorgito',
+      subnick: 'h3h3 xD',
+      email: 'jor_gito@live.com',
+      age: 16,
+      friend: true,
+      uid: 3
+    };
+    let coco: User = {
+      nick: 'Coco',
+      subnick: 'Estan todas buenas menos mi jermu',
+      email: 'basilecoco89@msn.com',
+      age: 28,
+      friend: true,
+      uid: 4
+    };
+    let hernan: User = {
+      nick: 'Herni uwu',
+      subnick: 'xd',
+      email: 'herniwi@awadeuwu.com',
+      age: 69,
+      friend: false,
+      uid: 5
+    };
+    this.friends = [
+      myUser,
+      eduardo,
+      jorge,
+      coco,
+      hernan
     ];
   }
 
