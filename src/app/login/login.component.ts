aa1e24bd90c6cc4316c776e06e75ff7d5f084759
Facundo Palombo <facundo.palombo@gmail.com>
2019-01-24 18:05:51 -0300

    Se crea el servicio de registro de usuario de firebase

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   operation: string = 'login';
-  constructor() { }
+  email: string = null;
+  password: string = null;
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+  }
+  login() {
+    this.authenticationService.loginWithEmail(this.email, this.password)
+      .then(
+        (data) => {
+          alert('Logeado correctamente');
+          console.log(data);
+        }
+      ).catch(
+        (error) => {
+          console.log('Ocurrio un error: ');
+          console.error(error);
+        }
+      );
+  }
+  register() {
+    this.authenticationService.registerWithEmail(this.email, this.password)
+      .then(
+        (data) => {
+          alert('Registrado correctamente');
+          console.log(data);
+        }
+      ).catch(
+        (error) => {
+          console.log('Ocurrio un error: ');
+          console.error(error);
+        }
+      );
   }
 
 }
