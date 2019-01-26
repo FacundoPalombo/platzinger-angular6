99efdde9398bd1e69896c35edf3c61109481146f
Facundo Palombo <facundo.palombo@gmail.com>
2019-01-26 11:37:44 -0300

    Implementamos un login con facebook

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,6 @@ import { AuthenticationService } from '../services/authentication.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  credential = firebase.auth.FacebookAuthProvider.credential(access_token);
   operation: string = 'login';
   email: string = null;
   password: string = null;
@@ -30,27 +29,13 @@ export class LoginComponent implements OnInit {
         }
       );
   }
-  loginWithFacebook() {
-    firebase.auth()
-      .signInAndRetrieveDataWithCredential(this.credential)
-      .catch(function (error) {
-        // Handle Errors here.
-        let errorCode = error.code;
-        let errorMessage = error.message;
-        // The email of the user's account used.
-        let email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        let credential = error.credential;
-        console.error(`
-      :-----:Error register:-----:
-      Code: ${errorCode},
-      Message: ${errorMessage},
-      Email: ${email},
-      Credentials: ${credential}
-      :--------------------------:
-      `);
-      });
-
+  async loginWithFacebook() {
+    try {
+      const data = await this.authenticationService.loginWithFacebook();
+      console.log('Login Succesfull with Facebook.');
+    } catch (error) {
+      console.error(error);
+    }
   }
   register() {
     this.authenticationService.registerWithEmail(this.email, this.password)
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
   constructor(private angularFireAuth: AngularFireAuth) { }
+  loginWithFacebook() {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    provider.setCustomParameters({
+      display: 'popup'
+    });
+    return this.angularFireAuth.auth.signInWithPopup(provider);
+  }
   loginWithEmail(email: string, password: string) {
     return this.angularFireAuth.auth.signInWithEmailAndPassword(email, password);
   }
