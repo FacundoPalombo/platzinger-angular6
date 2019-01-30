630cc0df331ce0440f4d5d37d6ca8df3e33eb30c
Facundo Palombo <facundo.palombo@gmail.com>
2019-01-30 17:47:13 -0300

    Configuramos el guard para que no se puedan acceder a las rutas sin estar logueado

--- src/app/services/authentication.guard.ts
+++ src/app/services/authentication.guard.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
+  constructor(private authenticationService: AuthenticationService) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
-  }
+      return this.authenticationService.getStatus().pipe(
+        map(status => {
+          if (status) {
+            return true;
+          } else {
+            return false;
+          }
+        })
+      );
+    }
 }
